344490a5d32083655aab1cc2859921346af800a3
Indrasish Roy <118255208+IndrasishRoy@users.noreply.github.com>
2024-10-28 15:25:03 +0530

    initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use client";
 
 import { useEffect } from 'react';
@@ -15,21 +14,3 @@ const RootPage = () => {
 };
 
 export default RootPage;
-=======
-"use client";
-
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-
-const RootPage = () => {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.push('/auth');
-  }, [router]);
-
-  return null;
-};
-
-export default RootPage;
->>>>>>> ae0f5209876ac3b090ee5a5da48891123d83dce4
